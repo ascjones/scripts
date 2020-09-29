2b6982582e0b0e3cb0a3bc886ed0e1bc186a33e2
Andrew Jones <ascjones@gmail.com>
2020-09-29 11:16:45 +0100

    Fetch rewards in pages, to allow accounts with > 100 records

--- rewards.sh
+++ rewards.sh
@@ -7,8 +7,8 @@ fetch_rewards () {
     --header 'Content-Type: application/json' \
     --data-raw '{
       "address": "'$ADDRESS'",
-      "page": 0,
-      "row": '$1'
+      "page": '$1',
+      "row": 10
     }'
 }
 
@@ -20,26 +20,34 @@ fetch_price () {
     }'
 }
 
-# fetch the count so we don't need to do paging
-COUNT=$(fetch_rewards 1 | jq '.data.count')
-
+# csv header
 echo "block_num,block_time,amount_dot,price_usd,price_time"
-for row in $(fetch_rewards "$COUNT" | jq -c '.data.list[]'); do
-  _jq() {
-    echo ${row} | jq -r ${1}
-  }
 
-  block_num=$(_jq '.block_num')
-  block_timestamp=$(_jq '.block_timestamp')
-  block_time=$(date -d @"$block_timestamp" +'%Y-%m-%d %H:%M:%S')
-  amount_planck=$(_jq '.amount')
-  amount_dot=$(awk -v amt="$amount_planck" 'BEGIN{printf "%.7f\n", amt/10000000000}')
-  price=$(fetch_price "$block_num")
-  price_usd=$(echo "$price" | jq '.data.records[0].price')
-  price_timestamp=$(echo "$price" | jq '.data.records[0].time')
-  price_time=$(date -d @"$price_timestamp" +'%Y-%m-%d %H:%M:%S')
+PAGE=0
 
-  echo "$block_num,\"$block_time\",$amount_dot,$price_usd,\"$price_time\""
+while true; do
+  REWARDS=$(fetch_rewards "$PAGE")
+  if (($(echo "$REWARDS" | jq '.data.list | length') == 0)); then
+    break
+  fi
+  for row in $(echo $REWARDS | jq -c '.data.list[]'); do
+    _jq() {
+      echo ${row} | jq -r ${1}
+    }
+
+    block_num=$(_jq '.block_num')
+    block_timestamp=$(_jq '.block_timestamp')
+    block_time=$(date -d @"$block_timestamp" +'%Y-%m-%d %H:%M:%S')
+    amount_planck=$(_jq '.amount')
+    amount_dot=$(awk -v amt="$amount_planck" 'BEGIN{printf "%.7f\n", amt/10000000000}')
+    price=$(fetch_price "$block_num")
+    price_usd=$(echo "$price" | jq '.data.records[0].price')
+    price_timestamp=$(echo "$price" | jq '.data.records[0].time')
+    price_time=$(date -d @"$price_timestamp" +'%Y-%m-%d %H:%M:%S')
+
+    echo "$block_num,\"$block_time\",$amount_dot,$price_usd,\"$price_time\""
+  done
+  PAGE=$((PAGE + 1))
 done
 
 
